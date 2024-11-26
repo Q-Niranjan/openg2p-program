8bfcb1f7e86bdeac3b27be10001db217f17d26de
Niranjan Kumar <niranjan882684@gmail.com>
2024-11-26 13:08:03 +0530

    Added new module g2p_program_role_access

---
+++ g2p_programs_role_access/__manifest__.py
@@ -0,0 +1,28 @@
+# Part of OpenG2P. See LICENSE file for full copyright and licensing details.
+{
+    "name": "G2p Programs Role Access ",
+    "category": "G2P/G2P",
+    "version": "17.0.0.0.0",
+    "sequence": 1,
+    "author": "OpenG2P",
+    "website": "https://openg2p.org",
+    "license": "LGPL-3",
+    "depends": [
+        "g2p_programs",
+        "web",
+        "base"
+    ],
+    "data": [
+      
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "g2p_programs_role_access/static/src/js/create_program.js",
+            "g2p_programs_role_access/static/src/xml/create_program_template.xml",
+        ],
+    },
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
---
+++ g2p_programs_role_access/static/src/js/create_program.js
@@ -0,0 +1,36 @@
+
+import {patch} from "@web/core/utils/patch";
+import {ListController} from "@web/views/list/list_controller";
+
+patch(ListController.prototype, {
+
+    setup() {
+        super.setup();
+        this.is_manager = false;
+        this.is_validator = false;
+        this.is_finance_validator = false;
+        this.is_program_cycle_approver = false;
+        this.check_role();
+    },
+
+    async check_role() {
+        const user = this.env.services.user;
+        try {
+
+            const [is_manager, is_validator, is_finance_validator,is_program_cycle_approver] = await Promise.all([
+                user.hasGroup("g2p_programs.g2p_program_manager"),
+                user.hasGroup("g2p_programs.g2p_program_validator"),
+                user.hasGroup("g2p_programs.g2p_finance_validator"),
+                user.hasGroup("g2p_programs.g2p_program_cycle_approver"),
+            ]);
+            
+            this.is_manager = is_manager;
+            this.is_validator = is_validator;
+            this.is_finance_validator = is_finance_validator;
+            this.is_program_cycle_approver = is_program_cycle_approver;
+
+        } catch (error) {
+            console.error("Error checking user roles:", error);
+        }
+    },
+});
